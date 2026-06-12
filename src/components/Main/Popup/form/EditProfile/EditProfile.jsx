83400c39b20a17f89faf24b663798e89b43182e2
Amairani Romero <12romeroamairani@gmail.com>
2026-06-12 13:00:58 -0600

    validation

--- src/components/Main/Popup/form/EditProfile/EditProfile.jsx
+++ src/components/Main/Popup/form/EditProfile/EditProfile.jsx
@@ -1,17 +1,33 @@
-import { useState, useContext } from 'react'; 
-import CurrentUserContext from '../../../../../contexts/CurrentUserContext'; 
+import { useState, useContext } from "react";
+import CurrentUserContext from "../../../../../contexts/CurrentUserContext";
 
 export default function EditProfile() {
   const { currentUser, handleUpdateUser } = useContext(CurrentUserContext); // Obtiene el objeto currentUser
 
   const [name, setName] = useState(currentUser.name); // Agrega la variable de estado para name
   const [description, setDescription] = useState(currentUser.about); // Agrega la variable de estado para description
+  const [nameError, setNameError] = useState("");
+  const [descriptionError, setDescriptionError] = useState("");
 
   const handleNameChange = (event) => {
+    if (event.target.value.length < 3) {
+      setNameError("Error");
+    } else {
+      setNameError("");
+    }
+
     setName(event.target.value); // Actualiza name cuando cambie la entrada
   };
 
   const handleDescriptionChange = (event) => {
+    if (event.target.value.length < 3) {
+      setDescriptionError(
+        "Error: debe tener más de 2 caracteres y menos de 40",
+      );
+    } else {
+      setDescriptionError("");
+    }
+
     setDescription(event.target.value); // Actualiza description cuando cambie la entrada
   };
 
@@ -21,44 +37,49 @@ export default function EditProfile() {
     handleUpdateUser({ name, about: description }); // Actualiza la información del usuario
   };
 
- return (
+  return (
     <form
-      className='popup__form'
-      name='profile-form'
-      id='edit-profile-form'
+      className="popup__form"
+      name="profile-form"
+      id="edit-profile-form"
       noValidate
-      onSubmit={handleSubmit}>
-      <label className='popup__label'>
+      onSubmit={handleSubmit}
+    >
+      <label className="popup__label">
         <input
-          className='popup__input popup__input_type_name'
-          id='owner-name'
-          maxLength='40'
-          minLength='2'
-          name='userName'
-          placeholder='Name'
+          className="popup__input popup__input_type_name"
+          id="owner-name"
+          maxLength="40"
+          minLength="2"
+          name="userName"
+          placeholder="Name"
           required
-          type='text'
+          type="text"
           value={name} // Bind name to input
           onChange={handleNameChange} // Agrega el controlador onChange
         />
-        <span className='popup__error' id='owner-name-error'></span>
+        <span className="popup__error" id="owner-name-error">
+          {nameError}
+        </span>
       </label>
-      <label className='popup__label'>
+      <label className="popup__label">
         <input
-          className='popup__input popup__input_type_description'
-          id='owner-description'
-          maxLength='200'
-          minLength='2'
-          name='userDescription'
-          placeholder='About me'
+          className="popup__input popup__input_type_description"
+          id="owner-description"
+          maxLength="200"
+          minLength="2"
+          name="userDescription"
+          placeholder="About me"
           required
-          type='text'
+          type="text"
           value={description} // Vincula description con la entrada
           onChange={handleDescriptionChange} // Agrega el controlador onChange
         />
-        <span className='popup__error' id='owner-description-error'></span>
+        <span className="popup__error" id="owner-description-error">
+          {descriptionError}
+        </span>
       </label>
-      <button className='button popup__button' type='submit'>
+      <button className="button popup__button" type="submit">
         Save
       </button>
     </form>
